984fe813f7abb5f69351bc0c29ff980067f5d588
lucaasgian-arch <lucaasgian@gmail.com>
2026-01-19 18:51:24 -0300

    script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 import { createClient } from "https://esm.sh/@supabase/supabase-js";
 
-const supabaseUrl = "SUA_SUPABASE_URL";
-const supabaseKey = "SUA_ANON_PUBLIC_KEY";
+const supabaseUrl = sb_publishable_adcIc_tXdio53_i1GjekqA_yaKkrav9;
+const supabaseKey = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJjdmd0anFmemt5ZG9zcmZ3bWR2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njg4NDQ5NzMsImV4cCI6MjA4NDQyMDk3M30.v7_-rSF1MFzSOwVdyESdKQhklfRTdZOPj_G0qRhiF5A;
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
